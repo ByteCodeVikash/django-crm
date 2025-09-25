2cd0f3db7c43fbb8fbcbe291ecedad2feecb03b1
Vikash dubey <112969269+ByteCodeVikash@users.noreply.github.com>
2025-09-25 15:17:41 +0530

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -1,11 +1,98 @@
+import os
+import dj_database_url
 from django.utils.translation import gettext_lazy as _
 from django.utils.safestring import mark_safe
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-secret-key-here')  # Render pe env var set karo
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'  # Render pe False rakho
+
+# Add Render domain to ALLOWED_HOSTS
+ALLOWED_HOSTS = ['django-crm-0rx9.onrender.com', 'localhost', '127.0.0.1']
+
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Add your custom apps here, jaise 'massmail', 'crm', etc.
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'crm.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'crm.wsgi.application'
+
+# Database
+# Use Render's PostgreSQL DATABASE_URL
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Your existing settings
 SHIPMENT_DATE_CHECK = True
 
-# List of fields, the value of which will be saved
-# to the Excel file when exporting contact persons.
 CONTACT_COLUMNS = [
     'first_name', 'last_name', 'title', 'sex', 'birth_date',
     'was_in_touch', 'phone', 'other_phone', 'mobile', 
@@ -14,17 +101,13 @@ CONTACT_COLUMNS = [
     'company', 'department', 'disqualified', 'massmail'
 ]
 
-# List of fields, the value of which will be saved
-# to the Excel file when exporting companies.
 COMPANY_COLUMNS = [
     'full_name', 'website', 'phone', 'city_name', 'address',
     'email', 'description', 'lead_source', 'was_in_touch',
-    'country',  'owner', 'type', 'industry', 'department',
+    'country', 'owner', 'type', 'industry', 'department',
     'disqualified', 'massmail'
 ]
 
-# List of fields, the value of which will be saved
-# to the Excel file when exporting leads.
 LEAD_COLUMNS = [
     'first_name', 'last_name', 'title', 'sex', 'birth_date',
     'was_in_touch', 'email', 'secondary_email', 'phone',
@@ -35,8 +118,6 @@ LEAD_COLUMNS = [
     'disqualified', 'massmail'
 ]
 
-# List of fields, the value of which will be saved
-# to the Excel file when exporting deals.
 DEAL_COLUMNS = [
     'request', 'contact', 'contact__email', 'contact__phone',
     'company', 'lead', 'lead__email', 'lead__phone',
@@ -44,7 +125,6 @@ DEAL_COLUMNS = [
 ]
 
 FIRST_STEP = _('Establish the first contact with the client.')
-
 
 CONVERT_REQUIRED_FIELDS = (
     'first_name', 'email',      # 'last_name'
